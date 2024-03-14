578ede2842f98fa2ae18f034d40d9cdfc2480618
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-03-14 07:09:17 -0500

    modified home page to show survey (will make into a component)

---
+++ frontend/app/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Survey from './Survey';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>Home</h1>
+      <Survey />
+    </div>
+  );
+}
