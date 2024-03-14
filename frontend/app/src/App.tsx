625e3a93413dbc4f1cbcfc74374977e152d06657
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-03-14 07:39:24 -0500

    updated a few files. App to include Routes and Home to have Navbar (was missing export statement) and index to have Router.

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Routes>
+            <Route path='/' Component={Home} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </header>
+      </div>
+    </Router>
   )
 }
 
--- frontend/app/src/pages/Home.tsx
+++ frontend/app/src/pages/Home.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Survey from './Survey';
-import './Home.css';
+import React from 'react'
+import Survey from '../components/Survey'
+import Navbar from '../components/Navbar'
+import './Home.css'
 
 function Home() {
   return (
     <div className='home'>
+      <Navbar /> {/* Include the Navbar component */}
       <h1>Home</h1>
       <Survey />
     </div>
-  );
+  )
 }
+
+export default Home
