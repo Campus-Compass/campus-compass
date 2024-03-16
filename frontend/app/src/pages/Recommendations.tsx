447d5ab6425bca00e17c196a503705df167ce36f
Alex Simon <aleandre6579.simon@gmail.com>
2024-03-16 17:19:42 +0700

    added container and aligned title

--- frontend/app/src/pages/Recommendations.tsx
+++ frontend/app/src/pages/Recommendations.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
 import { Typography } from '@mui/material'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import '../css/Recommendations.css'
 
 function Recommendations() {
   return (
-    <Typography variant='h1' component='h2'>
-      Recommendations
-    </Typography>
+    <React.Fragment>
+      <Typography variant='h2' component='h2' sx={{ textAlign: 'center' }} m={10}>
+        Recommendations
+      </Typography>
+
+      <Container maxWidth='xl'>
+        <Box mt={10} sx={{ bgcolor: 'white', height: '100vh', borderRadius: '50px 50px 0 0' }} />
+      </Container>
+    </React.Fragment>
   )
 }
 
