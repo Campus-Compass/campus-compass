7453130eca348d208a12237a4d9b5126b0317fea
Alex Simon <aleandre6579.simon@gmail.com>
2024-03-16 17:02:59 +0700

    installed mui library

--- frontend/app/src/routes/index.tsx
+++ frontend/app/src/routes/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
+import Recommendations from '../pages/Recommendations'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const Routes = () => {
   const routesForPublic = [
     {
       path: '/',
       element: <Home />
+    },
+    {
+      path: '/rec',
+      element: <Recommendations />
     }
   ]
 
