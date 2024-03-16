0e174ab5e40f6e710e17f5d958577e453345973f
Alex Simon <aleandre6579.simon@gmail.com>
2024-03-16 17:02:59 +0700

    installed mui library

---
+++ frontend/app/src/pages/Recommendations.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Typography } from '@mui/material'
+
+function Recommendations() {
+  return (
+    <Typography variant='h1' component='h2'>
+      Recommendations
+    </Typography>
+  )
+}
+
+export default Recommendations
