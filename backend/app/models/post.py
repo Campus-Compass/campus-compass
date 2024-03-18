c676e9d1492e5361e87c28aa731fdf45a6fbbac6
thanawinboon <pokepat27@gmail.com>
2024-03-18 16:07:20 +0700

    fixed column naming

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -8,7 +8,7 @@ class Post(SQLModel, table=True):
     """Post entity."""
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    user: Optional[int] = Field(default=None, foreign_key="service.id")
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
     title: str
     content: str
     created_at: datetime = Field(default_factory=datetime.now)
