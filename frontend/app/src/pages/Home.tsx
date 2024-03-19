a2de4c7e72150c8ada95080a9dcc2b0506c050df
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-03-19 09:33:12 -0500

    updated home page. Can't figure out how to call API and post json

--- frontend/app/src/pages/Home.tsx
+++ frontend/app/src/pages/Home.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-// import Survey from '../components/Survey' #todo
 import Navbar from '../components/Navbar'
+import Survey from '../components/Survey'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+// import Container from '@mui/material/Container'
+import { useTheme } from '@mui/material'
 import '../css/Home.css'
 
 function Home() {
+  const theme = useTheme()
   return (
     <div className='home'>
       <Navbar />
-      <h1>Home</h1>
-      {/*<Survey />*/}
+      <React.Fragment>
+        <Box m={10} color={theme.palette.primary.main}>
+          <Typography variant='h2' sx={{ textAlign: 'center', fontWeight: 500 }}>
+            Recommendations
+          </Typography>
+          <Typography variant='body2' sx={{ textAlign: 'center' }}>
+            Here are a list of services we recommend for you based on your answers.
+          </Typography>
+        </Box>
+      </React.Fragment>
+      {<Survey />}
     </div>
   )
 }
