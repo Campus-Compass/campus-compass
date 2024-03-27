19339f876b842ac6aed9675e4102e4a28b00a3b7
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-03-26 23:13:55 -0500

    began creation of Surveys page

---
+++ frontend/app/src/pages/Surveys.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Survey from '../components/Survey'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import { useTheme } from '@mui/material'
+import '../css/Home.css'
+
+function Surveys() {
+  const theme = useTheme()
+  return (
+    <React.Fragment>
+      <Box m={10} color={theme.palette.primary.main}>
+        <Typography variant='h2' sx={{ textAlign: 'center', fontWeight: 500 }}>
+          Services Recommendation Survey
+        </Typography>
+        <Typography variant='body2' sx={{ textAlign: 'center' }}>
+          Please answer the following so we may direct you to the proper services.
+        </Typography>
+      </Box>
+      <Container disableGutters sx={{ bgcolor: 'white', height: '100%', borderRadius: '50px 50px 0 0' }}>
+        <Survey />
+      </Container>
+    </React.Fragment>
+  )
+}
+
+export default Surveys
