cd927b45c77f247e43d7f680527c6aaa3d63ba07
thanawinboon <pokepat27@gmail.com>
2024-04-02 23:34:29 +0700

    [not tested] you can now get and edit service profile

--- backend/app/apis/service.py
+++ backend/app/apis/service.py
@@ -4,6 +4,8 @@ from app.apis.base_api import base_user_api
 from app.db import get_session
 from app.models.user import User, UserRole
 from app.schemas.auth import BaseAuthDetails
+from app.schemas.profile import BaseServiceProfile
+from app.services.service import ServiceService
 from app.utils.auth import get_auth_handler
 from fastapi import APIRouter, Depends, status
 from sqlmodel import Session
@@ -35,3 +37,25 @@ async def register_service(
     # user_id: str = Depends(auth_handler.auth_wrapper),
 ) -> User:
     return base_user_api.user_register(user_info, UserRole.SERVICE, session)
+
+
+@router.get(
+    "/profile",
+    tags=["service"],
+    status_code=status.HTTP_200_OK,
+)
+async def get_service_profile(
+    service_profile: BaseServiceProfile,
+    session: Session = Depends(get_session),
+    user_id: str = Depends(auth_handler.auth_wrapper),
+) -> Dict:
+    return ServiceService(session).get_service_info_by_user_id(service_profile, user_id)
+
+
+@router.post("/profile", tags=["service"], status_code=status.HTTP_202_ACCEPTED)
+async def edit_service_profile(
+    service_profile: BaseServiceProfile,
+    session: Session = Depends(get_session),
+    user_id: str = Depends(auth_handler.auth_wrapper),
+) -> Dict:
+    return ServiceService(session).edit_service_profile(service_profile, user_id)
